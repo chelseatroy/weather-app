8f7ca4c95890ec04042ef6125b4de80e388cd0db
Chelsea <chelsea.dommert@gmail.com>
2014-07-15 21:20:26 -0500

    We think it'll work this time

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,10 @@ class PagesController < ApplicationController
   def weather
     @results = Geocoder.search(request.remote_ip)
 
-    weather_info = OpenWeather::Current.city("#{@results.city}, #{@results.region_name}")
+    weather_info = OpenWeather::Current.city("#{@results[0].data["city"]}, #{@results[0].data["region_name"]}")
 
     @temp_celsius = (weather_info["main"]["temp"].to_f-273).round(2)
     @temp_farenheit = 1.8*@temp_celsius + 32
-
-
   end
 
 end
