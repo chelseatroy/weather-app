d454a28f75dbdc959836d45537f8217da76b1899
Chelsea <chelsea.dommert@gmail.com>
2014-07-15 20:54:09 -0500

    change the syntax again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   def weather
     @results = Geocoder.search(request.remote_ip)
 
-    weather_info = OpenWeather::Current.city("#{@results["city"]}, #{@results["region_name"]}")
+    weather_info = OpenWeather::Current.city("#{@results.city}, #{@results.region_name}")
 
     @temp_celsius = (weather_info["main"]["temp"].to_f-273).round(2)
     @temp_farenheit = 1.8*@temp_celsius + 32
